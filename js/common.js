ac148826127669a39b4eec9ff0e5d4776b2a7039
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2015-10-25 18:29:15 +0200

    all is done

--- js/common.js
+++ js/common.js
@@ -1,7 +1,29 @@
 
 	$(function() {
 
- var dsf
+		goToElement ();
  
 		
 	});
+
+
+	function goToElement () {
+
+		$(".js-goto").click(function(e) {
+
+			var self = $(this),
+				targetClass = self.data('goto');
+			    target = $(targetClass);
+
+			var speed = 1000;
+
+			 e.preventDefault();
+
+
+		    $('html, body').stop().animate({
+		        scrollTop: target.offset().top
+		        
+		    }, speed, 'easeInOutCubic');
+		});
+
+	} // goToElement
